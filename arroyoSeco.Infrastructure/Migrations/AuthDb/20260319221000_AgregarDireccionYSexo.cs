e23594468f1ff0d699c7cce9f3f7fe3879b4f0c0
David Tovar <davidtovarangel@gmail.com>
2026-03-19 15:27:34 -0600

    fix auth migration metadata for direccion/sexo

--- arroyoSeco.Infrastructure/Migrations/AuthDb/20260319221000_AgregarDireccionYSexo.cs
+++ arroyoSeco.Infrastructure/Migrations/AuthDb/20260319221000_AgregarDireccionYSexo.cs
@@ -1,9 +1,13 @@
 using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using arroyoSeco.Infrastructure.Auth;
 
 #nullable disable
 
 namespace arroyoSeco.Infrastructure.Migrations.AuthDb
 {
+    [DbContext(typeof(AuthDbContext))]
+    [Migration("20260319221000_AgregarDireccionYSexo")]
     public partial class AgregarDireccionYSexo : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
